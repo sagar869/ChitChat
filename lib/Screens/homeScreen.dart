6570b9f1db9561fefe464d6fe8d8ecef4c438ebb
sagar869 <sagar8692090524@gmail.com>
2021-08-20 21:29:26 +0530

    null safety

--- lib/Screens/homeScreen.dart
+++ lib/Screens/homeScreen.dart
@@ -2,7 +2,6 @@
 import 'package:chitchat/Screens/ChatRoom.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:google_sign_in/google_sign_in.dart';
